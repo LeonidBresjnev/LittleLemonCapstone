6bb1d74ceb85cae30a5190c268a18e64093258d9
LeonidBresjnev <simonsen@postkasse.org>
2023-02-06 20:26:35 +0100

    data fetchet!

--- app/src/main/java/com/example/littlelemon/Hero.kt
+++ app/src/main/java/com/example/littlelemon/Hero.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Hero(searchstring : String, changesearchstring : (String) -> Unit) {
+
         Column(
             modifier = Modifier
                 .background(color = MaterialTheme.colors.primary)
--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -2,18 +2,17 @@ package com.example.littlelemon
 
 import android.content.ContentValues
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.Glide
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 
@@ -23,18 +22,59 @@ fun Home(databaseitems: List<MenuItemRoom>) {
 
     var searchstring by remember { mutableStateOf("") }
     Log.d(ContentValues.TAG, "Home")
+    var menuItems = if (searchstring.isNotEmpty()) {
+        databaseitems.filter{ it.title.contains(searchstring,ignoreCase=true) }
+    } else {
+    databaseitems
+   }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    //if category is 0, then show all,
+    // if category is not 0, then show specific category
+    var category by remember { mutableStateOf(value=0) }
+    if (category != 0)
+
+    Column {
         Hero(searchstring = searchstring ,
             changesearchstring = { searchstring = it })
+
+        Column() {
+            Text(modifier=Modifier
+                .align(alignment = Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.h3,
+                text = stringResource(R.string.orderfordelivery))
+
+            Row(modifier=Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+
+                Button(colors = ButtonDefaults.buttonColors(if (category == 1) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary),
+                    onClick = { category = if (category == 0) 1 else 0  }) {
+Text(modifier = Modifier.background(if (category == 1) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary),
+    text="Starters")
+                }
+                Button(colors = ButtonDefaults.buttonColors(if (category == 2) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary),
+                    onClick = { category = if (category == 0) 2 else 0 }) {
+                    Text(modifier = Modifier.background(if (category == 2) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary), text="Mains")
+                }
+                Button(colors = ButtonDefaults.buttonColors(if (category == 3) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary),
+                    onClick = { category = if (category == 0) 3 else 0 }) {
+                    Text(modifier = Modifier.background(if (category ==3) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary), text="Dessert")
+                }
+                Button(colors = ButtonDefaults.buttonColors(if (category == 4) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary),
+                    onClick = { category = if (category == 0) 4 else 0 }) {
+                    Text(modifier = Modifier.background(if (category == 4) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.primary), text="Drinks")
+                }
+            }
+        }
+
+        Divider(color = MaterialTheme.colors.onPrimary, thickness = 2.dp)
 
         LazyColumn(
             modifier = Modifier
                 .fillMaxHeight()
-                .padding(top = 20.dp)
+                .padding(top = 20.dp), contentPadding = PaddingValues(5.dp)
         ) {
             items(
-                items = databaseitems,
+                items = menuItems,
                 itemContent = { menuItem ->
                    //val glide = Glide.with(this)
                     Column {
@@ -64,6 +104,8 @@ fun Home(databaseitems: List<MenuItemRoom>) {
                                 contentDescription = "food picture"
                             )
                         }
+
+                        Divider(color = MaterialTheme.colors.onPrimary, thickness = 1.dp)
                     }
                 }
             )
